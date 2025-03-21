7c2597099ae9e31501ac41ec16cf3b67d3bb8fc8
bribrisk <nguyenbrian96@gmail.com>
2025-03-20 23:09:42 -0400

    Almost done with this part

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from textnode import *
+from textnode import TextNode, TextType
 
 def main():
-    new_object = TextNode("anchor text", "link", "www.boot.dev")
+    new_object = TextNode("anchor text", TextType.LINK, "www.boot.dev")
     print(new_object)
+    print("Hello World")
 
-
+if __name__ == "__main__":
+    main()
